fb326fb055a6099b6c7ce26b8d2ea3f7170ca8a6
frederik7741 <frederiksbox@gmail.com>
2020-04-16 09:34:39 +0200

    c# start

---
+++ my-console-app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace my_console_app
+{
+    class Glazercalc
+    {
+      
+
+        static void Main(string[] args)
+        {
+            double width;
+            double woodlength;
+            width = 5;
+            double height = 4;
+            double glassArea;
+            woodlength = (width + height) * 2 * 3.25;
+          
+            glassArea = ( width * height )*2 ;
+
+            string value = "The length of the wood is " + woodlength + " feet";
+            Console.WriteLine(value);
+            Console.WriteLine( "The area of the glass is " +
+            glassArea + " square metres" ) ;
+        }
+    }
+}
