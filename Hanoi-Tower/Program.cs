6a70c7e73e895acfca337dc099c739fd5382ffc4
frederik7741 <frederiksbox@gmail.com>
2020-10-02 11:00:01 +0200

    https://github.com/frederik7741/C-Sharp-opg.git

---
+++ Hanoi-Tower/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hanoi_Tower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
